3075103d25a4b6de19e99c303231b3052bd60105
LeeYejin013 <remitjoy0413@gmail.com>
2025-03-31 22:57:27 +0900

    refactor: name과 score 사용 부분을 Getter 메소드로 호출하여 사용

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,33 +1,32 @@
 package racingcar;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class Race {
-    void race(int tryNumber, ArrayList<Car> cars) {
-        int n = cars.size();
+    public static void race(int tryNumber, List<Car> cars) {
         for (int i = 0; i < tryNumber; i++) {
-            for (Car car : cars){
+            for (Car car : cars) {
                 car.move();
-                System.out.println(car.name+" : "+car.score);
+                System.out.println(car.getName() + " : " + car.getScore());
             }
             System.out.println("\n");
         }
     }
 
-    void result(ArrayList<Car> cars){
-        ArrayList<String> winners = new ArrayList<>();
-        int n  = cars.size();
+    public static void result(List<Car> cars) {
+        List<String> winners = new ArrayList<>();
         int maxScore = 0;
-        for (Car car : cars){   //maxScore설정
-            if (car.score.length() > maxScore){
-                maxScore = car.score.length();
+        for (Car car : cars) {   //maxScore설정
+            if (car.getScore().length() > maxScore) {
+                maxScore = car.getScore().length();
             }
         }
-        for(Car car : cars){    //maxScore인 차량들 저장
-            if (car.score.length() == maxScore){
-                winners.add(car.name);
+        for (Car car : cars) {    //maxScore인 차량들 저장
+            if (car.getScore().length() == maxScore) {
+                winners.add(car.getName());
             }
         }
-        System.out.println("최종 우승자 : "+String.join(",",winners));
+        System.out.println("최종 우승자 : " + String.join(",", winners));
     }
 }
