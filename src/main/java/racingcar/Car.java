fcafaca95398f765184c7d310642701a8ffcd001
LeeYejin013 <remitjoy0413@gmail.com>
2025-03-31 22:49:31 +0900

    fix: 인스턴스 변수의 접근 제한자 변경 및 Getter 메소드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,19 +3,27 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    protected String name;
-    protected String score;
-    public Car(String name){
-        if(name.length() > 5){
-            throw new IllegalArgumentException();
-        }
+    private final String name;
+    private String score;
+
+    public Car(String name) {
         this.name = name;
         this.score = "";
     }
-    void move(){
-        int n = Randoms.pickNumberInRange(0,9);
-        if (n >= 4){
+
+    public void move() {
+        int n = Randoms.pickNumberInRange(0, 9);
+        if (n >= 4) {
             score += "-";
         }
     }
+
+    public String getScore(){
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
