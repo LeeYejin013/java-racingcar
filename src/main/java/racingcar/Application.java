e4295fd9ddd6b648df02938579ddd53675eea6dc
LeeYejin013 <remitjoy0413@gmail.com>
2025-03-31 22:58:57 +0900

    refactor: carNaming 메소드를 사용하여 객체 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,27 +1,23 @@
 package racingcar;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-
 import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.print("경주할 자동차들의 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분):");
         String names = readLine();
-        String[] nameList = names.split(",");
-        ArrayList<Car> cars = new ArrayList<>();
-        for (String name : nameList) {
-            Car car = new Car(name);
-            cars.add(car);
-        }
+        List<Car> cars = CarName.carNaming(names);
 
         System.out.print("시도할 회수는 몇회인가요?");
         int tryNumber = Integer.parseInt(readLine());
 
-        Race race = new Race();
-        race.race(tryNumber, cars);
-        race.result(cars);
+        Race.race(tryNumber, cars);
+        Race.result(cars);
     }
 }
